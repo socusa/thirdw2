7e4f4d6a1af02bfdc9affbe3f54e9f08583891c7
Keith Lynn <klynn@southalabama.edu>
2019-06-12 06:17:04 +0100

    Commit

---
+++ app/src/main/java/com/example/lynn/third/MyView.java
@@ -0,0 +1,14 @@
+package com.example.lynn.third;
+
+import android.content.Context;
+import android.widget.RelativeLayout;
+
+import static com.example.lynn.third.MainActivity.*;
+
+public class MyView extends RelativeLayout {
+
+    public MyView(Context context) {
+        super(context);
+    }
+
+}
